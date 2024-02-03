d7993d76c434a546b7ca13d68402f9c55d826676
Anfal12345 <sulemanbhashadodmni@gmail.com>
2024-02-04 00:49:35 +0530

    Githubuser file is added

---
+++ Router/my-project/src/component/GitHub/GithubUser.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useState,useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
+function GithubUser() {
+    const username = useParams()
+    const [data,setData] = useState([])
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await fetch(`https://api.github.com/users/${username}`);
+            const userData = await response.json();
+            setData(userData);
+          } catch (error) {
+            console.error('Error fetching GitHub user:', error);
+          }
+        };
+    
+        fetchData();
+      }, [username]);
+    return (
+        
+          <div className='text-center m-4 bg-slate-300 text-white p-4 '>
+       Github Followers {data.followers} 
+       <img src={data.avatar_url} alt="" width={300} className='mx-auto pt-7' />
+    </div>
+        
+    )
+}
+
+export default GithubUser
