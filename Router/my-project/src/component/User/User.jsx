e83df37f39b4216739520e85e36241ffccbc853a
Anfal12345 <sulemanbhashadodmni@gmail.com>
2024-02-04 01:17:24 +0530

    User fileis added

--- Router/my-project/src/component/User/User.jsx
+++ Router/my-project/src/component/User/User.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom'
 function User() {
     const {userid}=useParams()
   return (
-    <div className='mx-auto text-center text-3xl bg-lime-200  mx-8  my-3'>
+    <div className=' text-center text-3xl bg-lime-200  mx-8  my-3'>
         User :{userid}
 
     </div>
