b64efa6dd8c2c72b2f0fcfe3df539165fef53539
Anfal12345 <sulemanbhashadodmni@gmail.com>
2024-02-04 21:18:31 +0530

    Array is addded which containing data to render

---
+++ React-testimonial/my-app/src/data.js
@@ -0,0 +1,41 @@
+import sulemanimg from './images/myphoto.jpg';
+import madhuImg from './images/madhu-1.jpg'
+import Anjumphoto from './images/Anjumphoto.jpg'
+const reviews=[
+    {
+        id:1,
+        name:"suleman",
+        job:"software engineeer",
+        image: sulemanimg,
+        text:"hi iam suleman . iam frm bangalore , currently iam doing nothing .this is my testimonial, and love to do code and currentky iam doing react js "
+    },
+    {
+        id:2,
+        name:"shaheed",
+        job:"Aerospace enginner",
+        image:"https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcT14xeuHgwvRv4MKwdR24nqQ6541sSRo1l3b-foKW_UQrU05Q8lWhgtVr9GMMVo1Lb3KHXAmwZoxu0UMQY",
+        text:"hi iam shaheed . iam frm bangalore , currently iam doing nothing .this is my testimonial, and love to do code and currentky iam doing react js "
+    },
+    {
+        id:3,
+        name:"Manoj Kumar ",
+        job:"civil engineeer",
+        image:"https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcT14xeuHgwvRv4MKwdR24nqQ6541sSRo1l3b-foKW_UQrU05Q8lWhgtVr9GMMVo1Lb3KHXAmwZoxu0UMQY",
+        text:" A Civil Engineer with excellent technical knowledge having a keen desire to achieve greater heights through  Career with an organization of high reputation."
+    },
+    {
+        id: 4,
+        name:"Madhu kumar",
+        job:"Civil Engineer",
+        image:madhuImg,
+        text :"Insightful Civil Engineer with diligent work ethic and excellent communication skills. Offering around 1 years of experience in India cost-effective delivery of   construction of bridge cum  Barrage, culverts, check dam, stone pitching. Comprehensive working knowledge of Site Supervision, Procurement, Quantity Surveying, Quality Checking and AutoCAD. "
+    },
+    {
+        id: 5,
+        name:"Anjum",
+        job:"Tech lead",
+        image:Anjumphoto,
+        text :"Insightful Civil Engineer with diligent work ethic and excellent communication skills. Offering around 1 years of experience in India cost-effective delivery of   construction of bridge cum  Barrage, culverts, check dam, stone pitching. Comprehensive working knowledge of Site Supervision, Procurement, Quantity Surveying, Quality Checking and AutoCAD. "
+    }
+];
+export default reviews; 
